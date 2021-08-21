8f68ec540c1de08ebf248f4431d896e3b317d767
CYNTAX <81620875+samuel-2001@users.noreply.github.com>
2021-08-21 22:56:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector(".buttonA").addEventListener("click", function () {
+  document.querySelector(".name").style.visibility = "hidden";
+});
+document.querySelector(".buttonB").addEventListener("click", function () {
+  document.querySelector(".name").style.visibility = "visible";
+});
